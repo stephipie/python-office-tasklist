a9555ddda3756b8b3a5a5972cf7636cd83b87073
stephipie <stephi.pietschmann@gmail.com>
2024-10-04 16:45:13 +0200

    enter discription to code

--- python-office-tasklist.py
+++ python-office-tasklist.py
@@ -7,30 +7,30 @@ def add_task():                 # define add function, ask for user input and se
         print(f"The task {task} was added to your list.")
     else:
         print("Your tasklist is empty.")
-add_task()
 
-def show_tasklist():
-    if office_tasklist == None:
+
+def show_tasklist():                        # define show tasklist function 
+    if office_tasklist == None:             # check if tasklist is empty
         print("Your tasklist is empty.")
     else:
-        for task in office_tasklist:
+        for task in office_tasklist:        # enter a for -loop to print the input of the user
             print("Your tasklist:")        
             print(task)
-show_tasklist()
 
-def main():
-    while True:
-        print("1. Add a task.")
+
+def main():                                 # define main function
+    while True:                             # while true do loop 
+        print("1. Add a task.")             # define options a user can choose
         print("2. Show the list.")
         print("3. End the programm.")
-        choice = input("Please make your choice. \n")
+        choice = input("Please make your choice. \n")   # ask user to make a choice
         if choice == "1":
             add_task()
         elif choice == "2":
             show_tasklist()
         elif choice == "3":
             print("Goodbye.")
-            break
+            break                           # break to end the while - loop
 if __name__ == "__main__":
     main()        
 
